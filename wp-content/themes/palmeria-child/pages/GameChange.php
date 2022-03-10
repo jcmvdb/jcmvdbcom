ee1a69a9f62d9ff652c8dd3cf911e953ad073aa2
jcmvdb <mvdb.jeroen@gmail.com>
2022-03-10 08:47:54 +0100

    footer and gamechange
    added footer and begin with the gamechange

--- wp-content/themes/palmeria-child/pages/GameChange.php
+++ wp-content/themes/palmeria-child/pages/GameChange.php
@@ -6,11 +6,60 @@
 $user = wp_get_current_user();
 get_header();
 
+$games = $wpdb->get_results('
+        SELECT * FROM `games` `g`
+        LEFT JOIN `Platform` `p`
+        ON `g`.`PlatformId` = `p`.`PlatformId`
+        LEFT JOIN `Form` `f`
+        ON `g`.`FormId` = `f`.`FormId`
+        WHERE 1
+');
+
 if (in_array('administrator', (array)$user->roles)) {
     ?>
 
     <h1 class="Title">Game Change</h1>
-    <p>Test</p>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Naam</th>
+            <th scope="col">Uitgever</th>
+            <th scope="col">Form</th>
+            <th scope="col">Platform</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $i = 1;
+        foreach ($games as $gamesItem) {
+            ?>
+            <tr>
+                <th scope="row"><?php echo $i; ?></th>
+<!--                <td>--><?php //echo $gamesItem->Name; ?><!--</td>-->
+                <input type="text" placeholder="<?php echo $gamesItem->Name ?>" value="<?php echo $gamesItem->Name ?>">
+                <td><?php echo $gamesItem->Developer; ?></td>
+                <td><?php echo $gamesItem->Form; ?></td>
+                <td><?php echo $gamesItem->Platform; ?></td>
+            </tr>
+            <?php
+            $i++;
+        }
+        ?>
+        </tbody>
+    </table>
+
+    <?php
+        foreach ($games as $gameItem) {}
+    ?>
+    <form action="">
+        <input type="text" placeholder="<?php echo $gamesItem->Name ?>" value="<?php echo $gamesItem->Name ?>" name="<?php echo $gamesItem->Name ?>">
+        <input type="text" placeholder="<?php echo $gamesItem->Name ?>" value="<?php echo $gamesItem->Name ?>" name="<?php echo $gamesItem->Name ?>">
+        <input type="text" placeholder="<?php echo $gamesItem->Name ?>" value="<?php echo $gamesItem->Name ?>" name="<?php echo $gamesItem->Name ?>">
+        <input type="text" placeholder="<?php echo $gamesItem->Name ?>" value="<?php echo $gamesItem->Name ?>" name="<?php echo $gamesItem->Name ?>">
+        <input type="submit" value="submit" name="submit">
+    </form>
 
     <?php
 } else {
@@ -26,10 +75,6 @@ if (in_array('administrator', (array)$user->roles)) {
 
                 <div class="page-content">
                     <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try search?', 'palmeria' ); ?></p>
-
-                    <?php
-                    get_search_form();
-                    ?>
                     <a href="<?php home_url();?>" class="button"><?php echo esc_html__('Go to home page', 'palmeria');?></a>
                 </div>
 
