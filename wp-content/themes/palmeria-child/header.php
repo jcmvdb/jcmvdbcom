bf1f4ed7a9692778eee0c9c4ed9d674f4c8d7bdf
jcmvdb <mvdb.jeroen@gmail.com>
2022-03-14 09:10:55 +0100

    font awesome
    added font awesome

--- wp-content/themes/palmeria-child/header.php
+++ wp-content/themes/palmeria-child/header.php
@@ -17,6 +17,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="profile" href="https://gmpg.org/xfn/11">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
 
     <?php wp_head(); ?>
 </head>
