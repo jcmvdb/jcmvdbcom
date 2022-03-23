c389bcaac6c05cf36f13edcfece61435478626cf
jcmvdb <mvdb.jeroen@gmail.com>
2022-03-23 13:45:27 +0100

    style
    changed some little things to make it nicer

--- wp-content/themes/palmeria-child/log-reg/login.php
+++ wp-content/themes/palmeria-child/log-reg/login.php
@@ -11,12 +11,12 @@ $user = wp_get_current_user();
 
 <?php
 if (!is_user_logged_in()) { // Display WordPress login form:
-    echo "<h1 class='text-center'> Login </h1>";
+    echo '<h1 class="Title">Login</h1>';
     $args = array(
 //        'redirect' => home_url(),
         'redirect' => '/account',
         'form_id' => 'loginform-custom',
-        'label_username' => __('Gebruikersnaam'),
+        'label_username' => __('Gebruikersnaam / Email'),
         'label_password' => __('Wachtwoord'),
         'label_remember' => __('Onthoud gegevens'),
         'label_log_in' => __('Log In'),
--- wp-content/themes/palmeria-child/pages/contact.php
+++ wp-content/themes/palmeria-child/pages/contact.php
@@ -5,14 +5,32 @@
 get_header();
 ?>
     <h1 class="Title">Contact</h1>
-    <p>Alle informatie over mij</p>
 
-
-    <div>
-        <h2>Social Media</h2>
-        <a href="https://twitter.com/Jcmvdb1"><i class="fab fa-twitter"> Twitter</i></a><br>
-        <a href="https://www.instagram.com/jcmvdb1/"><i class="fab fa-instagram"> Instagram (account staat privé)</i></a><br>
-        <a href="https://www.twitch.tv/jcmvdb"><i class="fab fa-twitch"> Twitch</i></a><br>
+    <h2>Social Media</h2>
+        <a href="https://twitter.com/Jcmvdb1" target="_blank"><i class="fab fa-twitter"> jcmvdb1</i></a><br>
+        <a href="https://www.instagram.com/jcmvdb1/" target="_blank"><i class="fab fa-instagram"> jcmvdb1 (account staat privé)</i></a><br>
+        <a href="https://www.twitch.tv/jcmvdb" target="_blank"><i class="fab fa-twitch"> jcmvdb</i></a><br>
+        <a href="" target="_blank"><i class="fab fa-linkedin"> jcmvdb</i></a><br>
+        <a href="" target="_blank"><i class="fab fa-github"> jcmvdb</i></a><br>
+        <p><i class="fab fa-mailchimp"> jcmvdb@gmail.com</i></p><br>
+<?php
+ for ($i = 0; $i <= 250; $i++) {
+     echo '<p><i class="fab fa-mailchimp"> jcmvdb@gmail.com</i></p><br>';
+ }
+?>
     </div>
+<!--    <div>-->
+<!--        <h2>Klachten of vragen?</h2>-->
+<!--        <form method="post">-->
+<!--            <label for="name">Naam</label>-->
+<!--            <input id="name" type="text" name="name" placeholder="naam" required><br>-->
+<!--            <label for="email">email</label>-->
+<!--            <input id="email" type="email" name="email" placeholder="email" required><br>-->
+<!--            <label for="klacht">Klacht/vraag</label>-->
+<!--            <input id="klacht" type="text" name="klacht" placeholder="klacht/vraag" required><br>-->
+<!---->
+<!--            <br><input type="submit" name="submit" value="submit">-->
+<!--        </form>-->
+<!--    </div>-->
 <?php
 get_footer();
