73fe0febc41f4d414299a090e8fcd20ede4ee6f3
jcmvdb <mvdb.jeroen@gmail.com>
2022-03-23 13:42:59 +0100

    footer
    added social media (twitter, instagram and twitch) in the footer

--- wp-content/themes/palmeria-child/footer.php
+++ wp-content/themes/palmeria-child/footer.php
@@ -15,9 +15,15 @@
 
 <footer id="colophon" class="site-footer">
     <div class="wrapper footer-wrapper">
-
+        <div class="text-center social-footer">
+            <a href="https://twitter.com/Jcmvdb1"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.instagram.com/jcmvdb1/"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.twitch.tv/jcmvdb"><i class="fab fa-twitch"></i></a>
+            <a href=""><i class="fab fa-linkedin"></i></a>
+            <a href=""><i class="fab fa-github"></i></a>
+        </div><br>
         <?php
-        if(has_nav_menu('menu-2')):
+        if (has_nav_menu('menu-2')):
             wp_nav_menu(array(
                 'theme_location' => 'menu-2',
                 'menu_class' => 'footer-menu',
@@ -25,7 +31,7 @@
             ));
         endif;
 
-        if(has_nav_menu('menu-3')):
+        if (has_nav_menu('menu-3')):
             wp_nav_menu(array(
                 'theme_location' => 'menu-3',
                 'menu_class' => 'theme-social-menu footer-socials',
@@ -38,19 +44,19 @@
         ?>
         <div class="site-info">
             <?php
-            $dateObj 		= new DateTime;
-            $current_year	= $dateObj->format( "Y" );
+            $dateObj = new DateTime;
+            $current_year = $dateObj->format("Y");
 
             printf(
-                wp_kses_post(get_theme_mod( 'palmeria_footer_text',
+                wp_kses_post(get_theme_mod('palmeria_footer_text',
                     sprintf(
-                        _x( '%2$s &copy; %1$s All Rights Reserved.<br> <span style="opacity: .8;">Powered by <a href="https://jcmvdb.com" rel="nofollow">jcmvdb</a>.</span>', 'Default footer text. %1$s - current year, %2$s - site title.', 'palmeria' ),
+                        _x('%2$s &copy; %1$s All Rights Reserved.<br> <span style="opacity: .8;">Powered by <a href="https://jcmvdb.com" rel="nofollow">jcmvdb</a>.</span>', 'Default footer text. %1$s - current year, %2$s - site title.', 'palmeria'),
                         $current_year,
-                        get_bloginfo( 'name' )
+                        get_bloginfo('name')
                     )
                 )),
                 $current_year,
-                get_bloginfo( 'name' )
+                get_bloginfo('name')
             );
 
             ?>
